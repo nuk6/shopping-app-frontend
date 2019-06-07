131a79eade1a1b3f41449d59ae5312c857183b0d
Nidhi Kumari <Nidhi.Kumari.met14@itbhu.ac.in>
2019-06-08 01:37:43 +0530

    further improvement

--- src/components/content-layout/Header.js
+++ src/components/content-layout/Header.js
@@ -4,7 +4,9 @@ export default class Header extends React.Component{
     render(){
         return(
             <div id='headerId'>
-                Header
+                <div className='headerContent'>
+                    Log in !!
+                </div>
             </div>
         );
     }
--- src/components/content-layout/SearchBar/SearchBar.js
+++ src/components/content-layout/SearchBar/SearchBar.js
@@ -4,21 +4,39 @@ class SearchBar extends React.Component{
     state = {
         listCollapsed : true
     }
+
+    handleChange = (e) => {
+        if(e.target.value.length >= 3){
+            this.setState({listCollapsed : false});
+        } else {
+            this.setState({listCollapsed : true});
+        }
+    }
+    handleBlur = (e) => {
+        this.setState({listCollapsed : true});
+    }
+    
+
     render(){
+        let res = [];
+        if(!this.state.listCollapsed){
+            for(let i = 0; i < 13; ++i){
+                res.push(
+                    <div className='searchItem' key='i' >
+                        <div id='searchItemResId' className='searchIt searchItemRes'></div>
+                    </div>
+                );
+            }
+        }
         return(
-            <div id='searchBarWrap'>
-                <div id='searchBarId'>
-                    <div>
-                    <input id='searchInput' placeholder='Search...'/>
-                    <input id='searchInput' placeholder='Search...'/>
+            <div id='searchBarBody'>
+               <div id='searchBarWrap'>
+                    <div className='searchItem'>
+                        <input id='id0' placeholder='Search...' spellCheck='false' className='searchIt searchItInput' type='text'onChange={this.handleChange} onBlur={this.handleBlur}/>
+                        <div className='searchButton'>Search</div>
                     </div>
-                    <div className='submitButtons'>
-                        Go
-                    </div>
-                </div>
-                <div id='searchBarListId'>
-                    
-                </div>
+                    {res}
+               </div>
             </div>
         );
     }
