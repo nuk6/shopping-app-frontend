86b52a2ab4cb7c5afd6f85a85512a1c0cd6fb2b5
Nidhi Kumari <Nidhi.Kumari.met14@itbhu.ac.in>
2019-06-07 23:28:52 +0530

    refactor

--- src/components/content-layout/SearchBar/SearchBar.js
+++ src/components/content-layout/SearchBar/SearchBar.js
@@ -1,14 +1,23 @@
 import React from 'react';
-require('./ContentLayout.css');
+require('../ContentLayout.css');
 class SearchBar extends React.Component{
+    state = {
+        listCollapsed : true
+    }
     render(){
         return(
             <div id='searchBarWrap'>
                 <div id='searchBarId'>
+                    <div>
                     <input id='searchInput' placeholder='Search...'/>
+                    <input id='searchInput' placeholder='Search...'/>
+                    </div>
                     <div className='submitButtons'>
                         Go
                     </div>
+                </div>
+                <div id='searchBarListId'>
+                    
                 </div>
             </div>
         );
--- src/components/pages/DisplayPage.js
+++ src/components/pages/DisplayPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SearchBar from '../content-layout/SearchBar';
+import SearchBar from '../content-layout/SearchBar/SearchBar';
 import Header from '../content-layout/Header';
 require('./Pages.css');
 export default class DisplayPage extends React.Component{
